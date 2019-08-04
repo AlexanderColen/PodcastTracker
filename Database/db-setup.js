8d9cd88e251aac70eeabd045dda0cfa4e87237fb
Alexander Colen <alexanderc@live.nl>
2019-08-04 15:27:38 -0400

    Fixed timezones for all Docker containers and added RSS feeds instead of names for Mongo DB setup.

--- Database/db-setup.js
+++ Database/db-setup.js
@@ -8,5 +8,5 @@ for (var i = 0; i < podcasts.length; i++) {
     }
 
     // Insert value into database.
-    db.rss.save({name: podcasts[i]});
+    db.rss.save({name: '', rss: podcasts[i], lastRefresh: Date()});
 }
