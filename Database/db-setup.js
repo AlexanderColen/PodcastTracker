266e11af5ca44c3a38624d4dcea032dbf810fdec
Alexander Colen <alexanderc@live.nl>
2019-08-03 22:27:47 -0400

    Prepared project start with Docker setup
    Start of database Docker setup with some test inputs.

---
+++ Database/db-setup.js
@@ -0,0 +1,12 @@
+var file = cat('./rss-feeds.txt');
+var podcasts = file.split('\n');
+
+for (var i = 0; i < podcasts.length; i++) {
+    // Remove \r from the end of the string.
+    if (podcasts[i].indexOf("\r") >= 0) {
+        podcasts[i] = podcasts[i].replace("\r", "");
+    }
+
+    // Insert value into database.
+    db.rss.save({name: podcasts[i]});
+}
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # PodcastTracker
+
+## Technologies
+The following technologies were used for this project:
+* Docker
+* Python 3
+* MongoDB
+
+_2019 - Alexander Colen_
