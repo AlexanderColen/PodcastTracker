4cfd58fef9047c9e5615ec2ef1bda72e06b59a33
Alexander Colen <alexanderc@live.nl>
2019-08-30 23:01:26 -0400

    Revert "Start of Dockerizing and removal of unnecessary ASP.NET functionalties."
    This reverts commit f0089a9cec15a4ca8892f8e763aa334a6780fa2d.

---
+++ Backend/Podcast-BE/Podcast-BE/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace Podcast_BE.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
